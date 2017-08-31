7b462eac0b03eadae5b82e5ac831710dd2be0619
Michael Fincham <michael@hotplate.co.nz>
2017-08-31 15:40:47 +1200

    Update paste.py

--- paste/paste.py
+++ paste/paste.py
@@ -7,7 +7,7 @@ Expects these environment variables to be set:
 
 PASTE_PATH = location where paste files will be stored, defaults to 'paste'
 PASTE_ID_LENGTH = number of characters in the paste ID, defaults to '16', which is around 95 bits of entropy
-PASTE_KEY_LENGTH = number of bytes in the paste ID, defaults to '32', which produces 256 bits of entropy
+PASTE_KEY_LENGTH = number of octets in the paste encryption key, defaults to '32', which produces 256 bits of entropy
 
 For most applications PASTE_ID_LENGTH and PASTE_KEY_LENGTH could be shortened, though PASTE_KEY_LENGTH cannot
 be shortened below 16.
